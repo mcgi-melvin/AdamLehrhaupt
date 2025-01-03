f5052b175b7543afeb3059be65eaf6bb17cc717d
Melvin <melvinlomibao@gmail.com>
2025-01-04 00:08:24 +0800

    Initial Files

---
+++ header.php
@@ -0,0 +1,341 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container" style="position: relative;">
+    <div class="menu_bar_1">
+        <a href="#">
+            <div class="img-container">
+                <img src="" alt="Logo">
+            </div>
+        </a>
+        <h1 class="head-text">ADAM LEHRHAUPT</h1>
+        <div class="icons-container">
+            <div class="icons">
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#ffffff" d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><path fill="#ffffff" d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 448 512"><path fill="#ffffff" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 320 512"><path fill="#ffffff" d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
+            </div>
+        </div>
+    </div>
+
+    <div class="menu_bar_2">
+        <ul class="ul_main">
+            <li><a href="https://adamlehrhaupt.com/home-new/">Home</a></li>
+            <li><a href="https://adamlehrhaupt.com/about-new/">About</a></li>
+            <li><a href="https://adamlehrhaupt.com/books-new/">Books</span></a>
+                <div class="dropdown_menu">
+                    <ul class="dropdown">
+                        <li><a href="#">Warning: Do Not Open This Book!</li>
+                        <li><a href="#">Please, Open This Book!</li>
+                        <li><a href="#">I Will Not Eat You</li>
+                        <li><a href="#">I Don't Draw, I Color</li>
+                        <li><a href="#">Chicken in Space</li>
+                        <li><a href="#">Chicken in School</li>
+                        <li><a href="#">Chicken in Mittens</li>
+                        <li><a href="#">Wordplay</li>
+                        <li><a href="#">This is a Good Story</li>
+                        <li><a href="#">Idea Jar</li>
+                        <li><a href="https://adamlehrhaupt.com/books-new/">View More Books &#8594;</li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="">Visits</a>
+                <div class="dropdown_menu">
+                    <ul class="dropdown">
+                        <li><a href="https://adamlehrhaupt.com/author-visit-new">Author Visit</li>
+                        <li><a href="https://adamlehrhaupt.com/school-visit/">School Visit</li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="">Guides/Activities</a>
+                <div class="dropdown_menu">
+                    <ul class="dropdown">
+                        <li><a href="https://adamlehrhaupt.com/teachers-guide/">Teacher's Guides</li>
+                        <li><a href="https://adamlehrhaupt.com/fun-activities-new/">Fun Activities</li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="https://adamlehrhaupt.com/events-new/">Events</li>
+            <li><a href="https://adamlehrhaupt.com/contact-new/">Contact</li>
+        </ul>
+        <div class="menu-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512"><path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+        </div>
+    </div>
+
+<style>
+
+    @import url('https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: ABeeZee, sans-serif;
+        font-weight: 300;
+    }
+
+    /*Header*/
+
+    .container {
+        width: 100%;
+        height: auto;
+    }
+    .head-text {
+        color: #FFFFFF;
+        font-size: 40px;
+        font-weight: normal;
+        font-family: Inria Serif, sans-serif;
+    }
+    .menu_bar_1 {
+        background-color: #3A6D8C;
+        height: 120px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 5%;
+        column-gap: 40px;
+    }
+    .menu_bar_2 {
+        background-color: #001F3F;
+        height: 70px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 5%;
+    }
+    .icons {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        column-gap: 40px
+    }
+    .menu_bar_2 ul {
+        list-style: none;
+        display: flex;
+    }
+    .menu_bar_2 ul li {
+        padding: 0 30px;
+        position: relative;
+    }
+    .menu_bar_2 ul li a {
+        color: rgba(255,255,255, 0.8);
+        text-decoration: none;
+        font-size: 20px;
+        transition: all 0.3s;
+        font-family: Poppins, sans-serif;
+    }
+    .menu_bar_2 ul li a:hover {
+        color: #6A9FCD;
+    }
+    .dropdown_menu > ul li a {
+        color: black;
+        font-size: 15px;
+        font-weight: normal;
+    }
+    .dropdown_menu {
+        display: none;
+    }
+    .menu_bar_2 ul li:hover .dropdown_menu {
+        display: block;
+        position: absolute;
+        left: 30px;
+        top: 100%;
+        background-color: #FFFFFF;
+    }
+    .menu_bar_2 ul li:hover .dropdown_menu ul {
+        display: block;
+        margin: 10px;
+    }
+    .menu_bar_2 ul li:hover .dropdown_menu ul li {
+        width: 25vh;
+        padding: 10px;
+    }
+    .menu-btn {
+        display: none;
+    }
+
+    /*Footer*/
+
+    .links {
+        text-decoration: none;
+        color: #FFFFFF;
+        font-size: 18px;
+    }
+    .links:hover {
+        color: #6A9FCD !important;
+    }
+    .submit-button:hover {
+        background-color: #6A9FCD !important;
+    }
+    footer, .right-container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        gap: 40px;
+        width: 75%;
+        padding: 50px 70px;
+    }
+    footer .left-logo-container {
+        width: 25% !important;
+        padding: 50px 0 !important;
+    }
+    footer, .logo-container, .box {
+        display: flex !important;
+        align-items: center !important;
+        justify-content: center !important;
+        padding: 30px !important;
+        width: 90% !important;
+        max-width: 500px !important;
+        height: 200px !important;
+        background-color: #fff !important;
+        border-radius: 0 0 20px 0 !important;
+    }
+    .copyright, .terms, .policy {
+        font-size: 13px !important;
+    }
+    .copyright-links-container > a:hover {
+        color: #6A9FCD !important;
+    }
+
+    /*img {*/
+    /*    display: none !important;*/
+    /*    height: auto !important;*/
+    /*    max-width: 100% !important;*/
+    /*    border-style: none !important;*/
+    /*}*/
+
+
+
+    @media only screen and (max-width: 640px) {
+        /*Header*/
+        .menu_bar_2 ul li  {
+            display: none;
+        }
+        .menu_bar_2 {
+            justify-content: flex-end;
+        }
+        .head-text {
+            display: none;
+        }
+        .menu-btn {
+            display: block;
+        }
+
+        /*Footer*/
+        .nav-container {
+            flex-direction: column !important;
+            row-gap: 5px !important;
+        }
+        .icon-flex-container, .links-container {
+            width: 100% !important;
+        }
+        .links {
+            font-size: 15px !important;
+        }
+        .nav-header, .social-text {
+            font-size: 17px !important;
+        }
+        .icons-container {
+            column-gap: 15px !important;
+        }
+        .input-email-container {
+            width: 200px !important;
+            font-size: 13px !important;
+            padding: 10px 15px !important;
+        }
+        .submit-button {
+            font-size: 14px !important;
+            padding: 12px 20px !important;
+        }
+        .form-container {
+            column-gap: 10px !important;
+        }
+        footer, .right-container {
+            padding: 50px 70px !important;
+        }
+        .copyright, .terms, .policy {
+            font-size: 11px !important;
+        }
+        .social-text {
+            padding-top: 30px !important;
+            padding-bottom: 10px !important;
+        }
+        .nav-header {
+            padding-bottom: 10px !important;
+        }
+    }
+
+    @media only screen and (max-width: 800px){
+
+        /*Header*/
+        .head-text {
+            font-size: 30px;
+        }
+        .menu_bar_2 ul li a {
+            font-size: 18px;
+        }
+        .menu_bar_2 ul li {
+            padding: 10px;
+        }
+        .icons {
+            column-gap: 30px!important;
+        }
+    }
+
+    @media only screen and (max-width: 992px) {
+
+        /*Footer*/
+        footer, .logo-container, footer, .right-container {
+            width: 100%;
+            justify-content: space-around;
+            text-align: center;
+        }
+        footer .logo-container {
+            padding: 15px 0 0;
+        }
+        .icon-links-container, .links-container {
+            align-items: center !important;
+        }
+        footer, .main-footer-container {
+            flex-wrap: wrap !important;
+        }
+        footer, .main-footer-container {
+            display: flex !important;
+            min-height: 400px !important;
+        }
+        footer, .logo-container {
+            width: 100% !important;
+            text-align: center !important;
+        }
+        .flex-container {
+            flex-wrap: wrap !important;
+        }
+        .left-logo-container {
+            width: 100% !important;
+        }
+    }
+
+    @media only screen and (max-width: 1366px) {
+
+        /*Footer*/
+        .icon-flex-container {
+            flex-direction: column;
+        }
+        
+    }
+
+</style>
