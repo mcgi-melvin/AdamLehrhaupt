2bbceb20d370361bb668c143cd99e1c45a696078
Melvin <melvinlomibao@gmail.com>
2025-01-17 19:19:28 +0800

    Initial Files

--- footer.php
+++ footer.php
@@ -25,7 +25,7 @@
                         </div>
                     </a>
                     <h5 class="social-text" style="color: #428BCA; font-size: 20px; padding-top: 40px; padding-bottom: 20px; font-weight: bold;">Social</h5>
-                    <div class="icons-container" style=" display: flex;column-gap: 20px;">
+                    <div id="icon-main" class="icons-container-3" style=" display: flex;column-gap: 20px;">
                         <?php if( $facebook = get_theme_mod('fl-social-facebook') ): ?>
                             <a href="<?= $facebook ?>" class="links"><svg xmlns="http://www.w3.org/2000/svg" height="26" width="26" viewBox="0 0 448 512"><path fill="#ffffff" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/></svg></a>
                         <?php endif; ?>
