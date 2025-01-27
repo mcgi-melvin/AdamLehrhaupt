5bd99f56c95eab22c5b779191fc7b5ccd4199435
Melvin <melvinlomibao@gmail.com>
2025-01-27 23:58:53 +0800

    Initial Files

---
+++ single-post.php
@@ -0,0 +1,284 @@
+<?php get_header();?>
+
+<div class="single-blog-main-container" style="width:100%; height:100%;">
+    <div class="single-blog-flex-container-main" style="display: flex; align-items: center; flex-direction: column; width: 100%; height: 100%;">
+        <div class="single-blog-flex-container-top" style="width: 83%; padding: 0;">
+            <div class="single-blog-left" style="display: flex; align-items: flex-start; flex-direction: column;">
+                <h1 class="header-t">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+                <div class="author-p" style="display: flex; justify-content: space-between; align-items: center; flex-direction: row; width: 100%;">
+                    <p>Posted by Adam Lehrhaupt</p>
+                    <p>January 21, 2025</p>
+                </div>
+                <div class="single-blog-image-container" style="background: #eaeaea; width: 100%; height: 40vh;">
+                    <img src="" alt="">
+                </div>
+                <div class="single-blog-icons-container" style="display: flex; justify-content: center; align-items: center; flex-direction: row; width: 100%;">
+                    <div class="share-container" style="display: flex; justify-content: flex-end; align-items: center; flex-direction: row; column-gap: 15px; padding: 20px 0 20px 0; width: 100%;">
+                        <p class="share-t" style="margin: 0; font-size: 18px; color: white;">Share with</p>
+                        <a href="#" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="32" width="28" viewBox="0 0 448 512"><path fill="#ffffff" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"></path></svg></a>
+                        <a href="#" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="32" width="28" viewBox="0 0 448 512"><path fill="#ffffff" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path></svg></a>
+                        <a href="#" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="32" width="28" viewBox="0 0 512 512"><path fill="#ffffff" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path></svg></a>
+                    </div>
+                </div>
+                <div class="single-blog-p-container">
+                    <p class="">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, accumsan et lorem quis, tincidunt egestas purus. Nunc viverra fringilla malesuada. Curabitur ac sollicitudin felis, eget dignissim purus. Sed rhoncus nec tellus sed auctor. In sed vestibulum sem. Vestibulum eget viverra metus, nec aliquet odio. Nam diam orci, posuere elementum pulvinar a, pretium imperdiet nibh. In rhoncus felis at nulla lacinia luctus. Sed pulvinar euismod porttitor. Nulla accumsan nibh nec volutpat rhoncus. Etiam dapibus sem sit amet dolor gravida lobortis. Nullam semper nunc mi, eget egestas risus auctor in. Praesent libero arcu, dignissim eget dui eget, euismod tincidunt leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam malesuada, arcu sed convallis vestibulum, mi eros semper nibh, et eleifend tortor ipsum nec diam. Phasellus semper lacinia dolor.
+                    </p>
+                </div>
+                <hr style="width: 100%; margin: 50px 0 50px 0;">
+                <h4 style="color: white; font-weight: bold">Comments</h4>
+                <div style="padding: 40px 0;">
+                    <div class="" style="display: flex; justify-content: flex-start; align-items: center; color: white; column-gap: 20px; flex-direction: row;">
+                        <img style="border-radius: 50%;" src="//www.gravatar.com/avatar/a4d2ccf59bd16f3e0590d44169d8864b?s=32&r=g&d=mm" alt="">
+                        <p style="color: white; margin: 0;">Melvin Lomibao <span style="padding-left: 10px;">says:</span></p>
+                    </div>
+                    <div style="display: flex; justify-content: center; align-items: flex-start; flex-direction: column; margin: 0 0 0 50px; row-gap: 20px;">
+                        <p style="color: #1953A0; margin: 10px 0 0 0;">January 27, 2025 at 10:15 pm</p>
+                        <p style="color: white; margin: 0;">Awesome</p>
+                    </div>
+                </div>
+                <hr style="width: 100%; margin: 50px 0 0 0;">
+            </div>
+        </div>
+
+        <div class="single-blog-flex-container-bottom" style="width: 83%; padding: 50px 0 0 0;">
+            <div id="related_post">
+                <div class="grid-container">
+                    <div class="related-items">
+                        <div class="pya-blog-item">
+                            <div class="pya-blog-item--header">
+                                <div class="basic-info">
+                                    <span class="category">Uncategorized</span>
+                                    <h3 class="post-head">Lorem ipsum dolor sit amet.</h3>
+                                    <span style="color: white;">By Adam Lehrhaupt</span>
+                                </div>
+                                <div class="thumbnail">
+                                    <a href="#">
+                                        <img src="" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="pya-blog-item--body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi doloremque fuga, illo illum in ipsa ipsam iusto libero maxime quasi rem sunt tenetur voluptate.</p>
+                            </div>
+                            <div class="pya-blog-item--footer">
+                                <a href="#">
+                                    <span>Read More</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="pya-blog-item">
+                            <div class="pya-blog-item--header">
+                                <div class="basic-info">
+                                    <span class="category">Uncategorized</span>
+                                    <h3 class="post-head">Lorem ipsum dolor sit amet.</h3>
+                                    <span style="color: #fff;">By Adam Lehrhaupt</span>
+                                </div>
+                                <div class="thumbnail">
+                                    <a href="#">
+                                        <img src="" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="pya-blog-item--body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi doloremque fuga, illo illum in ipsa ipsam iusto libero maxime quasi rem sunt tenetur voluptate.</p>
+                            </div>
+                            <div class="pya-blog-item--footer">
+                                <a href="#">
+                                    <span>Read More</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="pya-blog-item">
+                            <div class="pya-blog-item--header">
+                                <div class="basic-info">
+                                    <span class="category">Uncategorized</span>
+                                    <h3 class="post-head">Lorem ipsum dolor sit amet.</h3>
+                                    <span style="color: #fff;">By Adam Lehrhaupt</span>
+                                </div>
+                                <div class="thumbnail">
+                                    <a href="#">
+                                        <img src="" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="pya-blog-item--body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi doloremque fuga, illo illum in ipsa ipsam iusto libero maxime quasi rem sunt tenetur voluptate.</p>
+                            </div>
+                            <div class="pya-blog-item--footer">
+                                <a href="#">
+                                    <span>Read More</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="pya-blog-item">
+                            <div class="pya-blog-item--header">
+                                <div class="basic-info">
+                                    <span class="category">Uncategorized</span>
+                                    <h3 class="post-head">Lorem ipsum dolor sit amet.</h3>
+                                    <span style="color: #fff;">By Adam Lehrhaupt</span>
+                                </div>
+                                <div class="thumbnail">
+                                    <a href="#">
+                                        <img src="" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="pya-blog-item--body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi doloremque fuga, illo illum in ipsa ipsam iusto libero maxime quasi rem sunt tenetur voluptate.</p>
+                            </div>
+                            <div class="pya-blog-item--footer">
+                                <a href="#">
+                                    <span>Read More</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<style>
+
+
+    /*Body*/
+
+    .single-blog-main-container {
+        background: #001F3F;
+        padding: 100px 0 0 0;
+    }
+
+    .header-t {
+        font-size: 30px;
+        color: #428BCA;
+        font-weight: bold;
+        margin: 10px 0;
+    }
+    .author-p > div > p {
+        font-size: 18px;
+        color: #fff;
+    }
+    .single-blog-p-container > p {
+        font-size: 16px;
+        color: #fff;
+        line-height: 1.4;
+        margin: 0;
+    }
+    .single-blog-image-container img {
+        height: 100%;
+        object-fit: cover;
+    }
+    .post-head {
+        color: #428BCA;
+        font-weight: bold;
+    }
+    .pya-blog-item--body > p {
+        margin-bottom: 0;
+    }
+    .author-p > p {
+        color: #fff;
+        font-size: 18px;
+    }
+
+    /*Mini Blogs*/
+   .related-items {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        gap: 40px;
+    }
+    .pya-blog-item {
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
+    .pya-blog-item--header {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+    .basic-info {
+        display: flex;
+        flex-direction: column;
+        gap: 1px;
+    }
+    .thumbnail {
+        width: 100%;
+        background: #fff;
+        height: 100%;
+        margin: 0;
+    }
+    .thumbnail:hover {
+        cursor: pointer;
+    }
+    .pya-blog-item--footer {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+    .pya-blog-item--footer > a {
+        text-decoration: none;
+        color: #6a9fcd;
+    }
+    .pya-blog-item--footer > a:hover {
+        color: #428BCA;
+    }
+    .category {
+        color: #6a9fcd;
+    }
+    .pya-blog-item--body {
+        color: #fff;
+    }
+    .grid-container {
+        padding: 0 0 50px 0;
+    }
+
+    @media (max-width: 640px) {
+        .header-t {
+            font-size: 22px;
+        }
+        .author-p > div > p {
+            font-size: 13px;
+        }
+        .share-t {
+            font-size: 13px !important;
+        }
+        .share-container {
+            column-gap: 10px;
+        }
+        .single-blog-p-container > p {
+            font-size: 14px;
+        }
+        .single-blog-main-container {
+            padding: 50px 0 0 0;
+        }
+        .author-p > p {
+            color: #fff;
+            font-size: 13px;
+        }
+        .share-container {
+            column-gap: 10px !important;
+        }
+
+
+    }
+    @media (max-width: 1025px) {
+        .post-head {
+            font-size: 20px;
+            margin: 10px 0;
+        }
+
+    }
+
+
+
+</style>
+
+<?php get_footer();?>
